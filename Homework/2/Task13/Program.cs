42fb519d0038ca978d37f8d069b6172a193b6ef3
uberdi <disinyakin@mail.ru>
2023-01-14 14:37:55 +0400

    edit task13 in homework2

--- Homework/2/Task13/Program.cs
+++ Homework/2/Task13/Program.cs
@@ -4,19 +4,37 @@
 // // 78 -> третьей цифры нет
 // // 32679 -> 6
 
+//правильное решение через остатки от деления и тд
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-if (-99>num)
-{   
-    string str = num.ToString();
-Console.WriteLine(str[3]);
+if (num<100)
+{
+    Console.WriteLine("У этого числа нет третьей цифры");
 }
 else if (num>99)
-{   
-    string str = num.ToString();
-Console.WriteLine(str[2]);
-}
-else 
 {
-    Console.Write("третьей цифры нет");
+    while(num>999)
+    {  
+        num=num/10;
+    }
+    Console.Write("Третья цифра: ");
+    Console.WriteLine(num%10);
 }
+
+//альтернативное решение через строки
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (-99>num)
+// {   
+//     string str = num.ToString();
+// Console.WriteLine(str[3]);
+// }
+// else if (num>99)
+// {   
+//     string str = num.ToString();
+// Console.WriteLine(str[2]);
+// }
+// else 
+// {
+//     Console.Write("третьей цифры нет");
+// }
