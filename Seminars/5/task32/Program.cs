945df803e17d3a3f77f89cff50f56911ff58d7e0
uberdi <disinyakin@mail.ru>
2023-01-16 21:40:34 +0400

    Fifth Seminar Completed

---
+++ Seminars/5/task32/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 32: Напишите программу замена элементов массива:
+//  положительные элементы замените
+//   на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] array = new int[12];//создаем массив из 12 нулей
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);//нули в массиве заменяем на цифры от -9 до 9
+    Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = -1 * array[i];
+    Console.Write(array[i]);
+}
+
+//какое то альтернативное решение (вообще ниче не понял)
+// int[] array = { -7, -4, 3, 4};
+// void PrintArray(int[]array)
+// {
+// int count = array.Length;
+
+// for (int index = 0; index < count; index++)
+// {
+// Console.Write($"{array[index]} ");
+
+// }
+
+// }
+// int[] SArray(int[] array1)
+// {
+// for (int index = 0; index < array.Length; index++)
+// {
+// array1[index]*=(-1);
+
+// }
+// return array1;
+// }
+
+// PrintArray(array);
+// int [] array2 = SArray(array);
+// Console.WriteLine();
+// PrintArray(array2);
---
+++ Seminars/5/task33/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 33: Задайте массив. 
+// Напишите программу, которая определяет,
+//  присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// int[] array = new int[6];
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(-9, 10);
+//     Console.WriteLine(array[i]);
+
+// if ( array[i]==num)
+// Console.WriteLine("Да");
+// else Console.WriteLine("Нет");
+// }
+
+// ++Вариант с вводом ключа Х
+
+int[] array = new int[6];
+int x=0;
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    Console.Write($"{array[i]} ");
+}
+for (int i = 0; i < array.Length; i++)
+{
+if ( array[i]==num)
+{
+x=1;
+}
+}
+if (x==1)
+{
+   Console.WriteLine("Да"); 
+}
+
+else Console.WriteLine("Нет");
