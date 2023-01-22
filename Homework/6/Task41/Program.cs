47dd50dac4e842d0499a4722747c41f29141d826
uberdi <disinyakin@mail.ru>
2023-01-22 11:42:37 +0400

    sixth homework done

--- Homework/6/Task41/Program.cs
+++ Homework/6/Task41/Program.cs
@@ -1,6 +1,22 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
-// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿// Задача 41: 
+// Пользователь вводит 
+// с клавиатуры M чисел.
+// Посчитайте, 
+// сколько чисел 
+// больше 0 ввёл пользователь.
 
-// 0, 7, 8, -2, -2 -> 2
-
+//  0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
+
+ Console.Write("Кол-во чисел: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int count = 0;
+    int[] array = new int[m];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Число " + Convert.ToString(i + 1) + " ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        if (array[i] > 0) count++;
+    }
+    Console.WriteLine("Кол-во чисел > 0: " + count);
--- Homework/6/Task43/Program.cs
+++ Homework/6/Task43/Program.cs
@@ -6,3 +6,20 @@
 // b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Clear();
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Точка пересечения с координатами ({x}; {y})");
